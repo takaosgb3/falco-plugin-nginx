f8d20ad4bfad166a99158780be74d0430cb3698c
takaosgb3 <takaosgb3@gmail.com>
2025-12-06 16:19:37 +0900

    fix: update plugin version from 0.3.0 to 1.4.0
    The plugin binary version displayed by `falco --list-plugins` was
    showing 0.3.0 instead of the release version. This fix updates the
    hardcoded version string to match the release version.

--- plugin/main.go
+++ plugin/main.go
@@ -119,7 +119,7 @@ func (n *NginxPlugin) Info() *plugins.Info {
 		Name:        "nginx",
 		Description: "Real-time nginx access log monitoring for security threats",
 		Contact:     "github.com/takaosgb3/falco-nginx-plugin",
-		Version:     "0.3.0",
+		Version:     "1.4.0",
 		EventSource: "nginx",
 	}
 }
